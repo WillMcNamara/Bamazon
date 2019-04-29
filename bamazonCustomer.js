7bf6cbf368155fb2f9f293d5bfdbb523c06c29eb
Will M <wmcna97@gmail.com>
2019-04-29 15:09:31 -0400

    added packages and intial code

---
+++ bamazonCustomer.js
@@ -0,0 +1,28 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    display();
+  });
+
+function display(){
+    var query = "SELECT * FROM products"
+    connection.query(query, function(err, res){
+        console.log(res);
+    })
+}
