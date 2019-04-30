c35e88be80e07008281460b6eac11bfe60921e1e
Will M <wmcna97@gmail.com>
2019-04-30 14:56:00 -0400

    reorganize into functions

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var cTable = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -23,6 +24,52 @@ connection.connect(function(err) {
 function display(){
     var query = "SELECT * FROM products"
     connection.query(query, function(err, res){
-        console.log(res);
+        var tempArr = [];
+        var items = [];
+        for (i = 0; i < res.length; i++){
+            tempArr.push(res[i]);
+            items.push(res[i].product_name);
+        }
+        console.table(tempArr);
+        itemPrompt(items, tempArr);
     })
+}
+
+function itemPrompt(items, tempArr){
+    inquirer.prompt([
+        {
+            message: "Which item would you like to purchase? [Press q to quit]",
+            name: "item",
+        }
+    ]).then(function(resp){
+        if (resp.item.toLowerCase() === "q"){
+            connection.end();
+        }
+        else{
+            quantityPrompt(items, tempArr, resp);
+        }
+    })
+}
+
+function quantityPrompt(items, tempArr, resp){
+    inquirer.prompt([
+        {
+            type: "number",
+            message: "How many would you like to buy?",
+            name: "amount",
+        }
+    ]).then(function(response){
+        console.log(items.indexOf(resp.item));
+        if (response.amount > tempArr[items.indexOf(resp.item)].stock){
+            console.log("Not enough in stock.");
+        }
+        else {
+            query = "UPDATE products SET stock = " + (tempArr[items.indexOf(resp.item)].stock - response.amount) + " WHERE product_name = " + response.item;
+            console.log(query);
+            connection.query(query, function(err, res){
+                console.log("Bought " + response.amount + " " + resp.item + "s!\n")
+            })
+        }
+        display();
+})
 }
